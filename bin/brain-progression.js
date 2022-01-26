90ce7fdee3e20cca7ec241938a474ac746b38ffe
Max Lyubimov <maxlubimov@gmail.com>
2022-01-26 18:17:31 +0300

    Add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+import _ from 'lodash';
+import runGame from '../index.js';
+import { randomNum } from '../src/lib.js';
+import dataConstructor from '../src/dataConstructor.js';
+
+const generateProgression = () => {
+  const start = randomNum(1, 50);
+  const step = randomNum(2, 10);
+  const length = randomNum(5, 15);
+
+  const progression = [start];
+  let counter = start;
+  do {
+    counter += step;
+    progression.push(counter);
+  } while (progression.length < length);
+
+  return progression;
+};
+
+const qaBuilder = () => {
+  const progression = generateProgression();
+  const hiddenValue = _.sample(progression);
+  const hiddenValueIndex = _.findIndex(
+    progression,
+    (idx) => idx === hiddenValue
+  );
+
+  progression[hiddenValueIndex] = '..';
+  const question = progression.join(' ');
+
+  return { question, answer: hiddenValue };
+};
+
+const gameData = dataConstructor(qaBuilder);
+const startMessage = 'What number is missing in the progression?';
+
+runGame(gameData, startMessage);
