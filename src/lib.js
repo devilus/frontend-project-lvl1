6b985362b2a87cffc1daf100824551fd79bd600b
Max Lyubimov <maxlubimov@gmail.com>
2022-01-26 18:12:22 +0300

    Update random generator

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
 
-export const randomNum = (maxNum = 100) => _.random(1, maxNum);
+export const randomNum = (minNum = 1, maxNum = 100) => _.random(minNum, maxNum);
 
 export const isEvenNum = (num) => num % 2 === 0;
