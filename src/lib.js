722551cb51fdde77ece5401f55545c26ae1243ae
Max Lyubimov <maxlubimov@gmail.com>
2022-01-26 13:40:28 +0300

    Keep common methods only

--- src/lib.js
+++ src/lib.js
@@ -1,20 +1,5 @@
 import _ from 'lodash';
-import { maxLevel } from '../index.js';
 
 export const randomNum = (maxNum = 100) => _.random(1, maxNum);
 
 export const isEvenNum = (num) => num % 2 === 0;
-
-export const gameDataBuild = (qaBuilder) => {
-  if (!_.isFunction(qaBuilder)) {
-    return [];
-  }
-
-  const gameData = [];
-  for (let i = 0; i < maxLevel; i += 1) {
-    const { question, answer } = qaBuilder();
-    gameData.push({ question, answer });
-  }
-
-  return gameData;
-};
